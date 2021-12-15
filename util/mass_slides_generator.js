d18f44bf15179e3f61cf273f691ac7cb1976f7f0
TimotheAlbouy <timothe.albouy@gmail.com>
2021-12-15 17:27:40 +0100

    fixed song padding

--- util/mass_slides_generator.js
+++ util/mass_slides_generator.js
@@ -8,70 +8,92 @@ const {
 } = require("./constants.js");
 
 module.exports = class MassSlidesGenerator {
+	constructor() {
+		this._songTitleSize = 30;
+		this._lyricsSize = 20;
+		this._xSongTitle = 5;
+		this._ySongTitle = 15;
+		this._interParagraphWidth = 10;
+		this._interParagraphHeight = 20;
+		this._songBoxMarginLeft = 10;
+		this._songBoxMarginRight = 10;
+		this._songBoxTop = this._ySongTitle + this._interParagraphHeight;
+	}
+
 	addPage() {
 		const width = this._doc.internal.pageSize.getWidth();
 		const height = this._doc.internal.pageSize.getHeight();
 		this._doc.addPage();
 		this._doc.setFillColor(33, 33, 33);
-		this._doc.rect(0, 0, width, height, 'F');
+		this._doc.rect(0, 0, width, height, "F");
 	}
 
 	addOrdinarySong(title, latinLyrics, frenchLyrics) {
 		const width = this._doc.internal.pageSize.getWidth();
-		this._doc.setFontSize(30);
-		this._doc.text(title, 10, 20);
-		this._doc.setFontSize(20);
+		this._doc.setFontSize(this._songTitleSize);
+		this._doc.text(title, this._xSongTitle, this._ySongTitle);
+		this._doc.setFontSize(this._lyricsSize);
+		const fullSongBoxWidth = width - this._songBoxMarginLeft - this._songBoxMarginRight;
+		const halfSongBoxWidth = (fullSongBoxWidth - this._interParagraphWidth) / 2;
+		const splitLatin = this._doc.splitTextToSize(latinLyrics, halfSongBoxWidth);
+		const splitFrench = this._doc.splitTextToSize(frenchLyrics, halfSongBoxWidth);
+		const xLatin = this._songBoxMarginLeft;
+		const xFrench = xLatin + halfSongBoxWidth + this._interParagraphWidth;
+		const yLanguages = this._songBoxTop;
+		const yLyrics = this._songBoxTop + this._interParagraphHeight;
 		this._doc.setFont(undefined, "italic");
-		this._doc.text("Latin", 20, 40);
-		this._doc.text("Français", width/2 + 10, 40);
+		this._doc.text("Latin", xLatin, yLanguages);
+		this._doc.text("Français", xFrench, yLanguages);
 		this._doc.setFont(undefined, "normal");
-		const splitLatin = this._doc.splitTextToSize(latinLyrics, width/2 - 30);
-		this._doc.text(splitLatin, 20, 60);
-		const splitFrench = this._doc.splitTextToSize(frenchLyrics, width/2 - 30);
-		this._doc.text(splitFrench, width/2 + 10, 60);
+		this._doc.text(splitLatin, xLatin, yLyrics);
+		this._doc.text(splitFrench, xFrench, yLyrics);
 	}
 
 	addNonOrdinarySong(title, lyrics) {
 		const width = this._doc.internal.pageSize.getWidth();
 		const height = this._doc.internal.pageSize.getHeight();
 		this._verseNumber = 1;
-		this._doc.setFontSize(30);
-		this._doc.text(title, 10, 20);
-		this._doc.setFontSize(20);
+		this._doc.setFontSize(this._songTitleSize);
+		this._doc.text(title, this._xSongTitle, this._ySongTitle);
+		this._doc.setFontSize(this._lyricsSize);
 		const lineHeight = this._doc.getTextDimensions('text').h;
+		const fullSongBoxWidth = width - this._songBoxMarginLeft - this._songBoxMarginRight;
+		const halfSongBoxWidth = (fullSongBoxWidth - this._interParagraphWidth) / 2;
 		if (lyrics.length == 1) {
-			const splitP1 = this._doc.splitTextToSize(lyrics[0], width - 40);
-			this.addParagraph(splitP1, 20, height/3);
+			const xP1 = this._songBoxMarginLeft;
+			const yP1 = this._songBoxTop;
+			const splitP1 = this._doc.splitTextToSize(lyrics[0], fullSongBoxWidth);
+			this.addParagraph(splitP1, xP1, yP1);
 		} else if (lyrics.length == 2) {
-			const splitP1 = this._doc.splitTextToSize(lyrics[0], width - 40);
-			const splitP2 = this._doc.splitTextToSize(lyrics[1], width - 40);
-			const xP1P2 = 20;
-			const yP1 = 40;
-			const yP2 = yP1 + splitP1.length * lineHeight + 20;
+			const splitP1 = this._doc.splitTextToSize(lyrics[0], fullSongBoxWidth);
+			const splitP2 = this._doc.splitTextToSize(lyrics[1], fullSongBoxWidth);
+			const xP1P2 = this._songBoxMarginLeft;
+			const yP1 = this._songBoxTop;
+			const yP2 = yP1 + splitP1.length * lineHeight + this._interParagraphHeight;
 			this.addParagraph(splitP1, xP1P2, yP1);
 			this.addParagraph(splitP2, xP1P2, yP2);
 		} else if (lyrics.length == 3) {
-			const splitP1 = this._doc.splitTextToSize(lyrics[0], width - 40);
-			const splitP2 = this._doc.splitTextToSize(lyrics[1], width - 40);
-			const splitP3 = this._doc.splitTextToSize(lyrics[2], width - 40);
-			const xP1P2P3 = 20;
-			const yP1 = 40;
-			const yP2 = yP1 + splitP1.length * lineHeight + 20;
-			const yP3 = yP2 + splitP2.length * lineHeight + 20;
+			const splitP1 = this._doc.splitTextToSize(lyrics[0], fullSongBoxWidth);
+			const splitP2 = this._doc.splitTextToSize(lyrics[1], fullSongBoxWidth);
+			const splitP3 = this._doc.splitTextToSize(lyrics[2], fullSongBoxWidth);
+			const xP1P2P3 = this._songBoxMarginLeft;
+			const yP1 = this._songBoxTop;
+			const yP2 = yP1 + splitP1.length * lineHeight + this._interParagraphHeight;
+			const yP3 = yP2 + splitP2.length * lineHeight + this._interParagraphHeight;
 			this.addParagraph(splitP1, xP1P2P3, yP1);
 			this.addParagraph(splitP2, xP1P2P3, yP2);
 			this.addParagraph(splitP3, xP1P2P3, yP3);
 		} else if (lyrics.length >= 4) {
-			const splitP1 = this._doc.splitTextToSize(lyrics[0], width/2 - 30);
-			const splitP2 = this._doc.splitTextToSize(lyrics[1], width/2 - 30);
-			const splitP3 = this._doc.splitTextToSize(lyrics[2], width/2 - 30);
-			const splitP4 = this._doc.splitTextToSize(lyrics[3], width/2 - 30);
-			const xP1P2 = 20;
-			const xP2P3 = width/2 + 10;
-			const yP1P3 = 40;
+			const splitP1 = this._doc.splitTextToSize(lyrics[0], halfSongBoxWidth);
+			const splitP2 = this._doc.splitTextToSize(lyrics[1], halfSongBoxWidth);
+			const splitP3 = this._doc.splitTextToSize(lyrics[2], halfSongBoxWidth);
+			const splitP4 = this._doc.splitTextToSize(lyrics[3], halfSongBoxWidth);
+			const xP1P2 = this._songBoxMarginLeft;
+			const xP2P3 = xP1P2 + halfSongBoxWidth + this._interParagraphWidth;
+			const yP1P3 = this._songBoxTop;
 			const yP2P4 = Math.max(
-				yP1P3 + splitP1 * lineHeight + 20,
-				yP1P3 + splitP3 * lineHeight + 20
+				yP1P3 + splitP1.length * lineHeight + this._interParagraphHeight,
+				yP1P3 + splitP3.length * lineHeight + this._interParagraphHeight
 			);
 			this.addParagraph(splitP1, xP1P2, yP1P3);
 			this.addParagraph(splitP2, xP1P2, yP2P4);
