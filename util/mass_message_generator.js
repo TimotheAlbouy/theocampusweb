46041b1d2b315c7edac982e863b28a42ee6e01dc
TimotheAlbouy <38692015+TimotheAlbouy@users.noreply.github.com>
2022-09-18 13:02:19 +0200

    Max 3 paragraphs for song in whatsapp message

--- util/mass_message_generator.js
+++ util/mass_message_generator.js
@@ -40,7 +40,6 @@ module.exports = function generateWhatsappMessage(
 	if (recessionalSong) {
 		if (firstPassed) whatsappMessage += "\n";
 		whatsappMessage += "\n\n" + generateWhatsappSong("Envoi", recessionalSong.title, recessionalSong.lyrics);
-		firstPassed = true;
 	}
 	
 	return whatsappMessage;
@@ -51,7 +50,7 @@ function generateWhatsappSong(type, title, lyrics) {
 	if (lyrics) {
 		const paragraphs = lyrics.split(/\r?\n\r?\n/);
 		let verseNumber = 1;
-		for (let paragraph of paragraphs) {
+		for (let paragraph of paragraphs.slice(0, 4)) {
 			whatsappSong += "\n";
 			const isChorus = paragraph.startsWith("R.");
 			if (!isChorus) paragraph = (verseNumber++) + ". " + paragraph;
